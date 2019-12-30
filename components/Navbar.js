21be4c667493943abb2358358992cfae5a29fb72
Nick <nicholas.zarate@gmail.com>
2019-12-30 11:09:45 -0700

    Change Navbar layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,35 +8,22 @@ class Navbar extends PureComponent {
     super(props)
     this.state = {
       tabs: ['about', 'projects', 'blog'],
-      active: props.path,
+      active: '',
     }
   }
 
   handleClick = (tab) => {
-    console.log("here")
-    console.log(tab)
     this.setState({
       active: tab
     });
   }
 
-  handleLogo = () => {
-    this.setState({
-      active: ''
-    })
-  }
-
   render() {
     return (
       <div style={ styles.container }>
-        <Link href={ '/' }>
-          <p onClick={ this.handleLogo } style={ styles.logo }>Z</p>
-        </Link>
-        <div style={ styles.tabs }>
-          { this.state.tabs.map((tab, index) =>
-            <Tab key={ index } onClick={ this.handleClick } active={ this.state.active === tab }>{ tab }</Tab>
-          )}
-        </div>
+        { this.state.tabs.map((tab, index) =>
+          <Tab key={ tab } tab={ tab } onClick={ this.handleClick } active={ this.state.active === tab }>{ tab }</Tab>
+        )}
       </div>
     )
   }
@@ -44,30 +31,14 @@ class Navbar extends PureComponent {
 
 let styles = {
   container: {
+    position: 'fixed',
+    zIndex: 2,
     display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: home,
+    flexDirection: 'column',
+    left: '4em',
+    top: '4em',
+    // backgroundColor: 'green'
   },
-  logo: {
-    flex: 1,
-    textAlign: 'center',
-    justifyContent: 'center',
-    borderStyle: 'solid',
-    borderWidth: '3px',
-    borderColor: '#4f4f4f',
-    borderRadius: '10px',
-    fontSize: 25,
-    fontWeight: 'bold',
-    padding: 5,
-    margin: '1.5% 1.5% 1.5% 1.5%',
-    color: '#4f4f4f'
-  },
-  tabs: {
-    display: 'flex',
-    flex: 29,
-    flexDirection: 'row',
-  }
 }
 
 export default Navbar
