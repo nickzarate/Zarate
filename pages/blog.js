3eb5362777d97873a7b2df0ada0b5aee81ce940b
Nick <nicholas.zarate@gmail.com>
2020-01-01 16:19:01 -0700

    Clean up page structure

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,5 @@
-import Layout from '../components/Layout'
-
 const Blog = () => (
-  <div className='blog flexrow'>
-    <h1 style={{ backgroundColor: 'gray', marginTop: 0 }}>Blog.</h1>
+  <div id='blog' className='flexrow'>
   </div>
 )
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,5 @@
-import Layout from '../components/Layout'
-
 const Projects = () => (
-  <div className='projects flexrow'>
-    <h1 style={{ backgroundColor: 'gray', marginTop: 0 }}>Projects.</h1>
+  <div id='projects' className='flexrow'>
   </div>
 )
 
