d6f56c321f5eda01a5d93417da3ab42664c047d7
Nick <nicholas.zarate@gmail.com>
2020-01-04 15:28:49 -0700

    Add contact page at the bottom

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import ContactForm from '../components/ContactForm'
+import Metrics from '../components/Metrics'
+
+const Contact = () => (
+  <div id='contact' className='row container'>
+    <ContactForm />
+    <Metrics />
+  </div>
+)
+
+export default Contact
